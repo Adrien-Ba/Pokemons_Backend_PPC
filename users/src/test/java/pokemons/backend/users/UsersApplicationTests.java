6fa06070b106c9646900b2e64f1b7f343d9d6333
Adrien-Ba <adrien.bassail@live.fr>
2022-03-29 09:56:50 +0200

    Initial Commit :tada:
    Services initialisation

---
+++ users/src/test/java/pokemons/backend/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package pokemons.backend.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
